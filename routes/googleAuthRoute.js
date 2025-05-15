0064373d643ed5e2fcf9cf45ba8115ff2a5843ff
abdessamademoussaif <161370224+abdessamademoussaif@users.noreply.github.com>
2025-05-15 05:14:18 +0100

    fix cookies

--- routes/googleAuthRoute.js
+++ routes/googleAuthRoute.js
@@ -19,28 +19,28 @@ router.get(
    
     res.cookie('authToken', token, {
       httpOnly: false,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false, //process.env.NODE_ENV === 'production'
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.cookie('profileImg', req.user.profileImg, {
       httpOnly: false,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false, //process.env.NODE_ENV === 'production'
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.cookie('userId', req.user._id.toString() , {
       httpOnly: false,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,  //process.env.NODE_ENV === 'production'
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.cookie('role', req.user.role, {
       httpOnly: false, 
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,  //process.env.NODE_ENV === 'production' 
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
